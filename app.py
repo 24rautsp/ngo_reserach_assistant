e05bac50e5bba9a234f8a5119ce2d26b9bc57366
unknown <24rautsp@rbunagpur.in>
2026-04-24 21:23:06 +0530

    fixed

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 from tavily import TavilyClient
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
